a7eb3e3cfb280a2f032b749cf7c6cfa49fc42590
Brian W Wiatrek <brianwiatrek@aol.com>
2024-09-28 11:11:21 -0500

    Initial commit

---
+++ src/StaffApp.java
@@ -0,0 +1,11 @@
+import java.sql.*;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class StaffApp extends Application {
+
+    public void start(Stage primaryStage) {
+
+    }
+
+}
